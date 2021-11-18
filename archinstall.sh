c7b2073ffe03ab38f88b38168cb201b3ff4aa170
IEatedASoup <88074866+IEatedASoup@users.noreply.github.com>
2021-11-18 10:00:22 -0600

    Add files via upload

---
+++ archinstall.sh
@@ -0,0 +1,16 @@
+timedatectl set-ntp true
+pacman -S figlet vim
+figlet set up your partitions
+cfdisk /dev/sda
+mkfs.ext4 /dev/sda
+mount /dev/sda2
+pacstrap /mnt base linux linux-firmware
+genfstab -U /mnt >> /mnt/etc/fstab
+arch-chroot /mnt
+locale-gen
+figlen enter hostname now
+vim /etc/hostname
+mkinitcpio -P
+figlet set root password
+passwd
+figlet ok, install your bootloader and you're good!
