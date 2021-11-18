b2f3d9eab9abc79cc19d5aad7744bc31f62ccd9b
IEatedASoup <88074866+IEatedASoup@users.noreply.github.com>
2021-11-18 11:24:13 -0600

    Update archinstall.sh

--- archinstall.sh
+++ archinstall.sh
@@ -15,7 +15,7 @@ mkinitcpio -P
 figlet set root password
 passwd
 figlet installing grub
-pacman -S grub efibootmgr
+pacman -S grub
 mkdir /boot/efi
 mount /dev/sda1 /boot/efi
 grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/boot/efi
