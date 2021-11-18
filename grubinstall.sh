0d2b5db6e099fb6cad0072160fccab6c6f91327c
IEatedASoup <88074866+IEatedASoup@users.noreply.github.com>
2021-11-18 11:40:51 -0600

    Update and rename archinstall.sh to grubinstall.sh

---
+++ grubinstall.sh
@@ -0,0 +1,5 @@
+mkdir /boot/efi
+mount /dev/sda1 /boot/efi
+grub-mkconfig -o /boot/grub/grub.cfg
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+pacman -Sy os-prober
